37800ef9c74215ff1f7badb6ddcd2e89fc6807e3
Ashish-Kr-01 <ashishkumar.ab01@gmail.com>
2025-12-05 23:16:29 +0530

    backend created

---
+++ src/backend/db.js
@@ -0,0 +1,32 @@
+import pg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config({path:"./.env"})
+
+const { Pool } = pg;
+
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error('DATABASE_URL not set in environment');
+}
+
+const pool = new Pool(
+    {
+        connectionString,
+        ssl: {rejectUnauthorized:false},
+        max:10,
+        idleTimeoutMillis: 30000,
+        connectionTimeoutMillis: 2000
+    }
+);
+
+pool.on("error", () => {
+    console.error("Unexpected pg error",err);
+}
+);
+
+export default pool;
+export async function query (text, params){
+    return pool.query(text, params);
+};
