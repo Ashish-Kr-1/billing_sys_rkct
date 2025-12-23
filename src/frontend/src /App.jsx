884c9e111c0839391de23ffd326f7a24b5e25932
AYUSH K <ayushk@AYUSHs-MacBook-Air.local>
2025-12-23 18:02:48 +0530

    taihenda

--- src/frontend/src /App.jsx
+++ src/frontend/src /App.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
 import { BrowserRouter , Routes , Route , Navigate } from 'react-router-dom'
 import Landing_page from './LandingPage.jsx'
 import Create_Party from './Create_Party.jsx'
 import Invoice from './Invoice.jsx'
 import Preview from './Preview.jsx'
-import CreateItem from './CreateItem.jsx'
+import Ledger from './Ledger.jsx'
 function App() {
   return (
     <div>
@@ -14,7 +13,7 @@ function App() {
           <Route path='/Create_Party' element={<Create_Party/>}/>
           <Route path='/Invoice' element={<Invoice/>}/>
           <Route path='/Preview' element={<Preview/>}/>
-          <Route path='/CreateItem' element={<CreateItem/>}/>
+          <Route path='/Ledger' element={<Ledger/>}/>
         </Routes>      
       </BrowserRouter>
     </div>
