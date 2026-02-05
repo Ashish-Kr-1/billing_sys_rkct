c0b8d5a402d014d390eb53ac0bcea7a7fac50803
AYUSH K <ayushk@AYUSHs-MacBook-Air.local>
2026-02-05 22:46:49 +0530

    logo done

--- src/frontend/src/components/Footer.jsx
+++ src/frontend/src/components/Footer.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
 function Footer() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   return (
     <div>
-        <div className='border-t-3 w-full border-[#004D43] text-white pt-5 bg-black mt-26 font-[Rubik]'>
-            <h1 className='cursor-pointer hover:underline underline-offset-2'>&copy; 2026 RK Casting & Engineering Works. All rights reserved.</h1>
-            </div>
+      <div className='border-t-3 w-full border-[#004D43] text-white pt-5 bg-black mt-2 font-[Rubik] '>
+        <h1 className='cursor-pointer hover:underline underline-offset-2 text-center'>&copy; 2026 RK Casting & Engineering Works. All rights reserved.</h1>
+      </div>
     </div>
   )
 }
